570ea3d7247afed95612e02806d38753376e5f90
Ashton Yoon <y109384@naver.com>
2018-06-09 22:45:41 +0900

    initial commit

---
+++ FormsRatingBar/FormsRatingBar.Android/RatingBarRenderer.cs
@@ -0,0 +1,47 @@
+﻿using Android.Content;
+using FormsRatingBar;
+using FormsRatingBar.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+using ARatingBar = Android.Widget.RatingBar;
+
+[assembly: ExportRenderer(typeof(RatingBar), typeof(RatingBarRenderer))]
+namespace FormsRatingBar.Droid
+{
+    class RatingBarRenderer : ViewRenderer<RatingBar, ARatingBar>
+    {
+        private ARatingBar aRatingBar;
+        private Context context;
+
+        public RatingBarRenderer(Context context) : base(context)
+        {
+            this.context = context;
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<RatingBar> e)
+        {
+            base.OnElementChanged(e);
+            
+            aRatingBar = new ARatingBar(context);
+
+            if (Element.IsEditable)
+                aRatingBar.IsIndicator = false;
+            else
+                aRatingBar.IsIndicator = true;
+
+            aRatingBar.NumStars = Element.CountOfStar;
+            aRatingBar.Rating = Element.Rating;
+            aRatingBar.StepSize = Element.StepSize;
+
+            aRatingBar.RatingBarChange += ARatingBar_RatingBarChange;
+
+            SetNativeControl(aRatingBar);
+        }
+
+        private void ARatingBar_RatingBarChange(object sender, ARatingBar.RatingBarChangeEventArgs e)
+        {
+            Element.Rating = e.Rating;
+        }
+    }
+}
---
+++ FormsRatingBar/FormsRatingBar/RatingBar.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace FormsRatingBar
+{
+    public class RatingBar : View
+    {
+        public static BindableProperty IsEditableProperty 
+            = BindableProperty.Create
+                (nameof(IsEditable),
+                typeof(bool),
+                typeof(RatingBar),
+                true);
+
+        public bool IsEditable
+        {
+            get => (bool)GetValue(IsEditableProperty);
+            set => SetValue(IsEditableProperty, value);
+        }
+
+        public static BindableProperty CountOfStarProperty
+            = BindableProperty.Create
+                (nameof(CountOfStar),
+                typeof(int),
+                typeof(RatingBar),
+                5);
+
+        public int CountOfStar
+        {
+            get => (int)GetValue(CountOfStarProperty);
+            set => SetValue(CountOfStarProperty, value);
+        }
+
+        public static BindableProperty RatingProperty
+            = BindableProperty.Create
+                    (nameof(Rating),
+                    typeof(float),
+                    typeof(RatingBar),
+                    0f,
+                    propertyChanged: (s, o, n) =>
+                    {
+                        (s as RatingBar).OnRatingUpdated((float)n);
+                    });
+
+        public float Rating
+        {
+            get => (float)GetValue(RatingProperty);
+            set => SetValue(RatingProperty, value);
+        }
+
+        public static BindableProperty StepSizeProperty
+            = BindableProperty.Create
+                    (nameof(StepSize),
+                    typeof(float),
+                    typeof(RatingBar),
+                    1f);
+
+        public float StepSize
+        {
+            get => (float)GetValue(StepSizeProperty);
+            set => SetValue(StepSizeProperty, value);
+        }
+
+        public event EventHandler<float> RatingUpdated;
+
+        protected virtual void OnRatingUpdated(float rating)
+        {
+            RatingUpdated?.Invoke(this, rating);
+        }
+    }
+}
